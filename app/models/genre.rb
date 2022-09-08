a82bf5a07dd981c40a994faef8c69a8b3795b199
Seth David Gyimah <esec26@gmail.com>
2022-09-08 21:55:27 +0000

    association methods

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,22 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    # artist_names = []
+    # self.artists.map do |item|
+    #   item.name
+    # end
+
+    self.artists.pluck(:name)
+    # Pluck returns an Array of attribute values type-casted to match the plucked column names,
   end
 end
